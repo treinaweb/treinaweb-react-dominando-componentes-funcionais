e3bf05f0c5c281caa26cd647095581e28503913e
hanashiro <felipeaah@gmail.com>
2021-11-03 10:11:32 -0300

    aula-02-02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+
+function Treinaweb() {
+    return <div>Olá</div>;
+}
+
+const Treinaweb2 = () => {
+    return <div>Olá2</div>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Treinaweb />
+            <Treinaweb2 />
+        </div>
+    );
 }
 
 export default App;
